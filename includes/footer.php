aaf62c58987472b2ee2c07da4b595a151a37aec6
Wajahat <wajahatalimay2001@gmail.com>
2021-07-29 13:05:58 -0700

    commit by wajahat

--- includes/footer.php
+++ includes/footer.php
@@ -79,7 +79,7 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
           </div>
         </div>
         <div class="col-md-6 col-md-pull-6">
-          <p class="copy-right">Copyright &copy; 2018 Bike Rental Portal. Brought To You By <a href=#>Abdul mOIZ</a></p>
+          <p class="copy-right">Copyright &copy; 2018 Bike Rental Portal. Brought To You By <a href=#>Wajahat & Moiz</a></p>
         </div>
       </div>
     </div>
